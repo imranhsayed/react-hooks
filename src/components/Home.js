7441f9dcf889f6d127f6f859f17bb8616b207dd1
Imran Sayed <imranhsayed@gmail.com>
2019-05-23 15:12:19 +0530

    Update image

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,12 +1,17 @@
 import React from 'react';
 import Nav from "./Nav";
+import ThorImage from '../../thor.png';
 
 const Home = () => {
 	return (
 		<>
 			<Nav/>
-			<div>
-				Welcome!!
+			<div className="mt-5 container">
+				<h2>Welcome!!</h2>
+
+				<div>
+					<img src={ThorImage} alt="Thor Image"/>
+				</div>
 			</div>
 		</>
 	);
