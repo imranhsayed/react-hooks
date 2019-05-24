032e406ab686ed51d28c4aebc0f3a7847f4f11e0
Imran Sayed <imranhsayed@gmail.com>
2019-05-24 15:19:53 +0530

    Update components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,7 @@
 import React from 'react';
-import Todo from "./components/Todo";
 import { Router } from "@reach/router";
 import Home from "./components/Home";
-import User from './components/User';
 import './Style.css';
-import SimpleCounter from "./components/SimpleCounter";
 import Hierarchical from "./components/Hierachical";
 
 
@@ -12,10 +9,7 @@ class App extends React.Component {
 	render() {
 		return (
 			<Router>
-				<Todo path="/todo"/>
 				<Home path="/"/>
-				<SimpleCounter path="/counter"/>
-				<User path="/user"/>
 				<Hierarchical path="/hierarchical"/>
 			</Router>
 		);
